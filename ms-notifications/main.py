c1e82c70c1895b837c1aead74c26b3b95890c757
daneralejandro03 <daneralejandro03@gmail.com>
2024-03-14 10:21:43 -0500

    first commit ms-notifications

---
+++ ms-notifications/main.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from azure.communication.email import EmailClient
+from flask import Flask, request, jsonify
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    correo = request.get_json()
+    try:
+        connection_string = os.environ.get("CONECTION_STRING")
+        client = EmailClient.from_connection_string(connection_string)
+
+        message = {
+            "senderAddress": os.environ.get("SENDER_ADRESS"),
+            "recipients": {
+                "to": [{"address": correo["address"]}],
+            },
+            "content": {
+                "subject": correo["subject"],
+                "plainText": correo["plainText"],
+            }
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+
+        return jsonify({"message": "Correo electrónico enviado correctamente"}), 200
+
+    except Exception as ex:
+        return jsonify({"error": str(ex)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
