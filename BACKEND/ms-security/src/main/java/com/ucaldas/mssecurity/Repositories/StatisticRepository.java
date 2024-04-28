cda41bb49312b3a16bb0117d60eb5f947ef1289c
daneralejandro03 <daneralejandro03@gmail.com>
2024-04-28 17:26:31 -0500

    Confirmation message

--- BACKEND/ms-security/src/main/java/com/ucaldas/mssecurity/Repositories/StatisticRepository.java
+++ BACKEND/ms-security/src/main/java/com/ucaldas/mssecurity/Repositories/StatisticRepository.java
@@ -4,7 +4,7 @@ import com.ucaldas.mssecurity.Models.Statistic;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-public interface StatisticRepository extends MongoRepository <Statistic, String> {
-    @Query ("{'user.$id':ObjectId(?0)")
+public interface StatisticRepository extends MongoRepository<Statistic, String> {
+    @Query("{'user.$id':ObjectId(?0)}")
     Statistic getStatisticByIdUser(String userId);
 }
