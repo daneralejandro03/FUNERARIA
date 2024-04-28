96958416881956feae7fac6c62e271f0d6165486
daneralejandro03 <daneralejandro03@gmail.com>
2024-04-28 10:57:34 -0500

    Daner project local fixes

--- ms-notifications/main.py
+++ ms-notifications/main.py
@@ -7,10 +7,18 @@ load_dotenv()
 
 app = Flask(__name__)
 
+def validate_email_data(data):
+    if "address" not in data or "subject" not in data or "plainText" not in data:
+        return False
+    return True
+
 @app.route('/send_email', methods=['POST'])
 def send_email():
     correo = request.get_json()
     print(correo)
+    if not validate_email_data(correo):
+        return jsonify({"error": "Datos de correo electrónico incompletos"}), 400
+
     try:
         connection_string = os.environ.get("CONNECTION_STRING")
         client = EmailClient.from_connection_string(connection_string)
@@ -31,8 +39,11 @@ def send_email():
 
         return jsonify({"message": "Correo electrónico enviado correctamente"}), 200
 
+    except KeyError as ex:
+        return jsonify({"error": "Campo faltante en los datos de correo electrónico: " + str(ex)}), 400
+
     except Exception as ex:
-        return jsonify({"error": str(ex)}), 500
+        return jsonify({"error": "Error al enviar el correo electrónico: " + str(ex)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
