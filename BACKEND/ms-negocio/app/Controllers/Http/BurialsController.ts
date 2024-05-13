29b9d116fe024c373af1a181ffe9cfea09eb7cb7
daneralejandro03 <daneralejandro03@gmail.com>
2024-05-13 01:59:22 -0500

    Correction of the Burial entity

--- BACKEND/ms-negocio/app/Controllers/Http/BurialsController.ts
+++ BACKEND/ms-negocio/app/Controllers/Http/BurialsController.ts
@@ -1,55 +1,51 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Burial from 'App/Models/Burial';
+import Burial from 'App/Models/Burial'
 
 export default class BurialsController {
-    //Create
-    public async store({ request }: HttpContextContract) {
-        const body = request.body();
-        const theBurial: Burial = await Burial.create(body);
-        return theBurial;
+  //Create
+  public async store({ request }: HttpContextContract) {
+    const body = request.body()
+    const theBurial: Burial = await Burial.create(body)
+    return theBurial
+  }
+
+  //Read
+  public async find({ request, params }: HttpContextContract) {
+    if (params.id) {
+      const theBurial = await Burial.findOrFail(params.id)
+      await theBurial.load('wakeRoom')
+      await theBurial.load('service')
+      return theBurial
+    } else {
+      const data = request.all()
+      if ('page' in data && 'per_page' in data) {
+        const page = request.input('page', 1)
+        const perPage = request.input('per_page', 20)
+        return await Burial.query().paginate(page, perPage)
+      } else {
+        return await Burial.query()
+      }
     }
+  }
 
+  //Update
+  public async update({ params, request }: HttpContextContract) {
+    const theBurial: Burial = await Burial.findOrFail(params.id)
+    const body = request.body()
+    theBurial.location = body.location
+    theBurial.cementery = body.cementery
+    theBurial.burial_date = body.burial_date
+    theBurial.wakeRoom = body.wakeRoom_id
+    theBurial.service = body.service_id
 
-    //Read
-    public async find({ request, params }: HttpContextContract) {
+    return await theBurial.save()
+  }
 
-        if (params.id) {
-            const theBurial = await Burial.findOrFail(params.id);
-            await theBurial.load('wakeRoom');
-            await theBurial.load('service');
-            return theBurial;
-        } else {
-            const data = request.all()
-            if ("page" in data && "per_page" in data) {
-                const page = request.input('page', 1);
-                const perPage = request.input("per_page", 20);
-                return await Burial.query().paginate(page, perPage)
-            } else {
-                return await Burial.query()
-            }
-        }
-    }
+  //Delete
+  public async delete({ params, response }: HttpContextContract) {
+    const theBurial: Burial = await Burial.findOrFail(params.id)
+    response.status(204)
 
-    //Update
-    public async update({ params, request }: HttpContextContract) {
-
-            const theBurial: Burial = await Burial.findOrFail(params.id);
-            const body = request.body();
-            theBurial.location = body.location;
-            theBurial.burial_type = body.burial_type;
-            theBurial.burial_date = body.burial_date;
-            theBurial.wakeRoom = body.wakeRoom_id;
-            theBurial.service = body.service_id
-
-            return await theBurial.save();
-        }
-
-        //Delete
-        public async delete({ params, response }: HttpContextContract) {
-
-            const theBurial: Burial = await Burial.findOrFail(params.id);
-            response.status(204);
-
-            return await theBurial.delete();
-        }
+    return await theBurial.delete()
+  }
 }
