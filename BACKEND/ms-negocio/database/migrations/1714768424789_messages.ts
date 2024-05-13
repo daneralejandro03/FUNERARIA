ba5e65148406276c6804d6ec19f7e7fb960bf088
daneralejandro03 <daneralejandro03@gmail.com>
2024-05-13 01:37:22 -0500

    messages migration fix

--- BACKEND/ms-negocio/database/migrations/1714768424789_messages.ts
+++ BACKEND/ms-negocio/database/migrations/1714768424789_messages.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('information', 255)
-      table.string('chat_id').unsigned().references('chats.id')
+      table.integer('chat_id').unsigned().references('chats.id').onDelete('CASCADE')
       table.string('user_id')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
@@ -16,5 +16,5 @@ export default class extends BaseSchema {
 
   public async down() {
     this.schema.dropTable(this.tableName)
-  } 
+  }
 }
