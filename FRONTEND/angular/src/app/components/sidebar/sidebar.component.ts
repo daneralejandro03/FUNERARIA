37f8ee28c8db6f454100467194b112a13bb823f8
JaimeCardona06 <jaime.cardona40549@ucaldas.edu.co>
2024-06-17 19:24:48 -0500

    menu development

--- FRONTEND/angular/src/app/components/sidebar/sidebar.component.ts
+++ FRONTEND/angular/src/app/components/sidebar/sidebar.component.ts
@@ -11,15 +11,27 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '2' },
+    /*{ path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '2' },
     { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '2' },
     { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '2' },
     { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '1' },
     { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '2' },
     { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '0' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '0' },
+    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '0' },*/
+    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '0' },
+    { path: '/administrators/list', title: 'Administrators', icon: 'ni-settings-gear-65 text-green', class: '1' },
+    { path: '/customers/list', title: 'Customers', icon: 'ni-badge text-info', class: '1' },
+    { path: '/employees/list', title: 'Employees', icon: 'ni-briefcase-24 text-red', class: '1' },
+    { path: '/incidents/list', title: 'Incidents', icon: 'ni-ambulance text-primary', class: '1' },
+    { path: '/owners/list', title: 'Owners', icon: 'ni-single-copy-04 text-warning', class: '1' },
+    { path: '/permissions/list', title: 'Permisos', icon: 'ni-key-25 text-success', class: '1' },
+    { path: '/plans/list', title: 'Plans', icon: 'ni-collection text-dark', class: '1' },
+    { path: '/rolepermissions/list', title: 'Roles - Permissions', icon: 'ni-key-25 text-warning', class: '1' },
+    { path: '/roles/list', title: 'Roles', icon: 'ni-hat-3 text-primary', class: '1' },
+    { path: '/services/list', title: 'Services', icon: 'ni-support-16 text-success', class: '1' },
+    { path: '/sites/list', title: 'Sites', icon: 'ni-building text-green', class: '1' },
+    { path: '/transfers/list', title: 'Transfers', icon: 'ni-send text-danger', class: '1' }
 
-    { path: '/administrators/list', title: 'Administrators', icon: '', class: '2' }
 ];
 
 @Component({
